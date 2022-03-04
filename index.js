35f529fdb68fef5b7d99fbf945062f27a31835b2
unknown <yahye@Yaxyash.10>
2022-03-04 08:14:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+{var age =20;
+var Name="Mumin"
+
+    // Let is used for Local sco
+console.log(`Name is : ${this.Name} age is: ${this.age}`)
+}
+// for(let i=0; i<5; i++ ){
+//     console.log(i);
+// }
+let list=[1,4,60,30,20,90];
+// console.log(list[1],list[5])
+
+// for(itenm of list)
+// console.log(itenm)
+
+// list.forEach(function(value){
+//     console.log(value)
+// })
+// for(index in list)
+// console.log("index "+index+" is "+list[index])
+
+let display=function(){
+    console.log("Hello world")
+}
+display();
+
+function Div(a,b){
+    return a/b;
+}
+console.log("Dive :"+Div(12,3))
